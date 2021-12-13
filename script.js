b6c0cf2bbc1096ec5716594d8f8e447037a4dcdc
River Diniz <river-s-diniz@hotmail.com>
2021-12-13 01:05:16 -0400

    Update script.js
    Correção do códigos.

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function criarBG() {
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green";
-        context.fillStyle(snake[i].x, snake[i].y, box, box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
